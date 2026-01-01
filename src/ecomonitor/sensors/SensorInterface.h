109ad6e6e6a2d1cca8aea8a07858dad8aa46015c
Galuxosi <jker1786@gmail.com>
2026-01-01 18:24:53 +0200

    Header comments on the most important files.

--- src/ecomonitor/sensors/SensorInterface.h
+++ src/ecomonitor/sensors/SensorInterface.h
@@ -1,3 +1,10 @@
+/*
+ * File: SensorInterface.h
+ * Description: Header file for connecting device logic with the sensors.
+ * Author: Andriy Tymchuk
+ * Created: 2025-12-17
+*/
+
 #pragma once
 
 #ifndef SENSOR_INTERFACE_H
